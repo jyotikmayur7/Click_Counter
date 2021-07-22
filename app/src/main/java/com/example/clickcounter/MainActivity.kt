637702b331d62eda4f18c93abe95851fb4e82c70
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-22 20:48:48 +0100

    Added onClicklistener on button to increment click counter

--- app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.clickcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counter : Int = 0
+        val clickCount: TextView = findViewById(R.id.click_count)
+        val showClickCount: Button = findViewById(R.id.show_click_count)
+
+        showClickCount.setOnClickListener{
+            clickCount.setText("Button Clicked ${++counter} times")
+        }
     }
 }
